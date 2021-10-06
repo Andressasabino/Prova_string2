b1524407e7bed600fd65daf2dbf1473054b8740b
Andressa_SabinoIFCE <84898051+Andressasabino@users.noreply.github.com>
2021-10-06 00:22:43 -0300

    Create q1.c

---
+++ q1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+  //declaração de variáveis
+  char fra[200];
+  int i, tam;
+  
+  //declaração da string
+  printf("Digite uma frase: ");
+  fgets(fra, 201, stdin);
+
+  //adquirindo o tamanho da string
+  tam = strlen(fra);
+
+  // fazer a frase inverter e mostrar
+  printf("Frase invertida: ");
+  for(i = tam; i >= 0; i--) {
+    printf("%c", fra[i]);
+  }
+ 
+  return 0;
+}
